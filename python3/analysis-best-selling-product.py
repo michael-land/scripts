6e995cb1f289023a631b6b86379cbbcba2a420d2
Michael Li <xiaoyu.tamu@gmail.com>
2019-06-05 11:11:32 -0500

    initial

---
+++ python3/analysis-best-selling-product.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import json
+from pprint import pprint
+
+order_items = list()
+with open("./order.json", "rb") as fin:
+    data = json.load(fin)
+
+    for order in data:
+        if "items" in order:
+            order_items.extend(order["items"])
+
+
+with open("./order-items.json", "w") as fout:
+    json.dump(order_items, fout, indent=4)
+
+
+df = pd.read_json("./order-items.json", orient="column")
+df["qty_ordered"] = df["qty_ordered"].astype(int)
+
+units_by_sku_sorted = (
+    df.groupby(["sku", "name"])["qty_ordered"]
+    .sum()
+    .reset_index(name="units")
+    .sort_values(by="units", ascending=False)
+)
+
+print(units_by_sku_sorted)
+units_by_sku_sorted.to_csv('./units_by_sku.csv')
+
+
